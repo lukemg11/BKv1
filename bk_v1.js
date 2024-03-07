e5aa5ce3d60aa9f13070395cdff116af643da849
lukemg11 <145138956+lukemg11@users.noreply.github.com>
2024-03-07 10:24:58 -0500

    Add files via upload

--- bk_v1.js
+++ bk_v1.js
@@ -1,24 +1,31 @@
-// Luke Greenberg, Period 3 and 4, Mr. Irimina
+// Luke Greenberg
+// Period 3 & 4 ODD days
+// Mr. Irimina
 const SHA256 = require('crypto-js/sha256');
 
-class Block{
-    constructor(index, timestamp, data, previousHash=""){
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = calculateHash();
-    }
+class Block {
+   constructor(index, timestamp, data, previousHash="") {
+       this.index=index;
+       this.timestamp=timestamp;
+       this.data=data;
+       this.previousHash=previousHash;
+       this.hash=this.calculateHash();
+   }
+   calculateHash() {
+       return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
+   }
+ 
 }
-calculateHash()
-{
-    return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
+
+class Blockchain {
+   constructor() {
+       this.chain=[];
+       for (let i = 0; i<this.chain.length; i++) {
+           console.log(this.chain[i].hash);
+       }
+   }
+   createGenesisBlock() {
+       return new Block(0, "03/01/2009", "Genesis Block", "0");
+   }
 }
-class Blockchain{
-    constructor(){
-        this.chain=[];
-    }
-    createGenesisBlock(){
-        return new Block(0, "03/01/2009", "Genesis Block", "0");
-    }
-}
+new Blockchain();
